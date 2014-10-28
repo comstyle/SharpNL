6f18589be128d91ceec56fe3f090fd90d08a27a8
knuppe <gustavoknuppe@gmail.com>
2014-10-28 19:03:43 -0200

    ToString added

--- src/SharpNL/Utility/Sequence.cs
+++ src/SharpNL/Utility/Sequence.cs
@@ -107,5 +107,17 @@ namespace SharpNL.Utility {
         }
         #endregion
 
+        #region . ToString .
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>
+        /// A string that represents the current object.
+        /// </returns>
+        public override string ToString() {
+            return string.Format("{0} {1}", Score, outcomes.ToDisplay());
+        }
+        #endregion
+
     }
 }
